97239d8d647262482a42699991d90e30080f3dac
NatanHerzog <natanherzog@gmail.com>
2025-04-22 22:40:09 -0400

    fdm for 1d heat equation is good

--- one_dimensional_heat_eqn.m
+++ one_dimensional_heat_eqn.m
@@ -3,18 +3,20 @@ close all
 clc
 
 length = 1;
-total_time = 2;
+total_time = 1;
 num_nodes = 101;
 thermal_diffusivity = 0.1;
 
 delta_x = length / (num_nodes - 1);
 
 delta_time = 0.5 * delta_x^2 / thermal_diffusivity; %? this is the stability condition, but where does it come from?
-num_time_iterations = ceil(total_time / delta_time);  %! round up to obey stability
+num_time_iterations = ceil(total_time / delta_time) + 1;  %! round up to obey stability
 delta_time = total_time / num_time_iterations;  %! redefine time step
 
+
+%% ! vectorized solution
 solution = zeros(num_time_iterations + 1 , num_nodes);
-solution(1 , 2:end-1 ) = sin( 0 + delta_x : delta_x : length - delta_x );
+solution(1 , 2:end-1 ) = cos( 2*pi * (0 + delta_x : delta_x : length - delta_x) ) + 1;
 
 
 for time_step = 2 : num_time_iterations
@@ -27,6 +29,32 @@ end
 
 figure(1)
 surf( time , position , solution' , "EdgeColor", "none")
+xlabel('$t$ [s]', 'Interpreter', 'latex')
+ylabel('$x$ [m]', 'Interpreter', 'latex')
+zlabel('$T$ [K]', 'Interpreter', 'latex')
+
+
+
+%% ! matrix solution
+
+solution_matrix = zeros(num_time_iterations + 1 , num_nodes);
+solution_matrix(1 , 2:end-1 ) = cos( 2*pi * (0 + delta_x : delta_x : length - delta_x) ) + 1;
+
+diagonal = ones(num_nodes,1) .* (1 - (2*thermal_diffusivity*delta_time/delta_x^2));
+off_diagonal = ones(num_nodes-1,1) .* (thermal_diffusivity*delta_time/delta_x^2);
+
+matrix = diag(diagonal) + diag(off_diagonal,-1) + diag(off_diagonal,1);
+
+for time_step = 2 : num_time_iterations
+
+  solution_matrix(time_step , :) = (matrix * solution_matrix(time_step-1 , :)')';
+  solution_matrix(time_step , 1) = 0;
+  solution_matrix(time_step , end) = 0;
+
+end
+
+figure(2)
+surf( time , position , solution_matrix' , "EdgeColor", "none")
 xlabel('$t$ [s]', 'Interpreter', 'latex')
 ylabel('$x$ [m]', 'Interpreter', 'latex')
 zlabel('$T$ [K]', 'Interpreter', 'latex')
